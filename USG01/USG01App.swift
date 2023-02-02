3a84b1a7cb687739f11c85a8feec3d1e07e73066
munchangjae <107340769+munchangjae@users.noreply.github.com>
2023-02-02 20:50:30 +0900

    usgtest1

---
+++ USG01/USG01App.swift
@@ -0,0 +1,17 @@
+//
+//  USG01App.swift
+//  USG01
+//
+//  Created by 문창재 on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct USG01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ USG01UITests/USG01UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  USG01UITestsLaunchTests.swift
+//  USG01UITests
+//
+//  Created by 문창재 on 2023/01/26.
+//
+
+import XCTest
+
+final class USG01UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
