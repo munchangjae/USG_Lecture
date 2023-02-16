9fb3f3e6aadea1df8ef3d906ad3f32aee09f5f91
munchangjae <107340769+munchangjae@users.noreply.github.com>
2023-02-16 19:40:25 +0900

    detailView 연습

---
+++ Practice/dataPractice/USG04App.swift
@@ -0,0 +1,17 @@
+//
+//  USG04App.swift
+//  USG04
+//
+//  Created by 백대홍 on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct USG04App: App {
+    var body: some Scene {
+        WindowGroup {
+            MovieListView()
+        }
+    }
+}
